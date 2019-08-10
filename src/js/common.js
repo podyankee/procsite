d14564f42885d6be34abe4a5d37d6317c1ab81d8
podyankee <podyankee@gmail.com>
2019-08-10 07:43:03 +0300

    first commit

---
+++ src/js/common.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next">&gt;</button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev">&lt;</button>',
+    infinite: false
+  })
+})
