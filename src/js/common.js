0db59e6b8b1501698c6fd237b96238391a0ca4ba
podyankee <podyankee@gmail.com>
2019-08-14 05:38:52 +0300

    finished coding

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,11 @@
 $(function () {
-  $('.slider__inner').slick({
+  $('.slider__inner, .news__slider-inner').slick({
     nextArrow: '<button type="button" class="slick-btn slick-next">&gt;</button>',
     prevArrow: '<button type="button" class="slick-btn slick-prev">&lt;</button>',
     infinite: false
   })
   $('select').styler()
+  $('.header__btn-menu').on('click', function methodName () {
+    $('.menu ul').slideToggle()
+  })
 })
